e2572446b67afbd2c2e857cd634fae900ef3001d
Никита Кубрик <kubriknd@outlook.com>
2023-11-19 16:02:04 +0500

    Add project files.

---
+++ Tyuiu.KubrikND.Sprint5.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KubrikND.Sprint5.Task0.V5.Lib;
+using System.IO;
+
+namespace Tyuiu.KubrikND.Sprint5.Task0.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Nikita\source\repos\Tyuiu.KubrikND.Sprint5\Tyuiu.KubrikND.Sprint5.Task0.V5\bin\Debug\OutPutFileTask0.txt";
+
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileexists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileexists);
+        }
+    }
+}
---
+++ Tyuiu.KubrikND.Sprint5.Task0.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KubrikND.Sprint5.Task0.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KubrikND.Sprint5.Task0.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9869e5f-d798-4e53-bdf6-5a25153c087b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
