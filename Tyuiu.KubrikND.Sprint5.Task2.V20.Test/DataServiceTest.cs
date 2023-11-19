eb897e8a01bd6704790c3ee427696a962d86bf13
Никита Кубрик <kubriknd@outlook.com>
2023-11-19 16:48:50 +0500

    Task2(v20)

---
+++ Tyuiu.KubrikND.Sprint5.Task2.V20.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.KubrikND.Sprint5.Task2.V20.Lib;
+
+namespace Tyuiu.KubrikND.Sprint5.Task2.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Test2()
+        {
+            string path = @"C:\Users\Nikita\source\repos\Tyuiu.KubrikND.Sprint5\Tyuiu.KubrikND.Sprint5.Task2.V20\bin\Debug\OutPutFileTask2.csv";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileex = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileex);
+        }
+    }
+}
---
+++ Tyuiu.KubrikND.Sprint5.Task2.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KubrikND.Sprint5.Task2.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KubrikND.Sprint5.Task2.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ffb20b0d-6ec5-4447-a65d-dc8ac6ad61be")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
